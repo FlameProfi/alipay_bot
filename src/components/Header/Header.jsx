62b78b9558d8475e40cbd2a711cf24add2027b19
beadidd <57589034+FlameProfi@users.noreply.github.com>
2024-11-11 00:00:49 +0300

    t

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
         <div className={'header'}>
             <Button onClick={onClose}>Закрыть</Button>
             <span className={'username'}>
-            Приветствую, {user.username}
+            Приветствую, {user?.first_name}
             </span>
         </div>
     );
