9e8ead8d617f8140aa00e718a5ac142847f33dff
beadidd <57589034+FlameProfi@users.noreply.github.com>
2024-11-14 14:22:11 +0300

    added back button commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import { useTelegram } from "../../hooks/useTelegram"
 import Button from "../Button/Button"
 import './Header.css'
 
 const Header = () => {
     const {user, onClose} = useTelegram();
+    const navigate = useNavigate();
 
     return (
         <div className={'header'}>
-            <Button onClick={onClose}>Закрыть</Button>
+            <Button onClick={() => navigate(-1)}>Назад</Button>
             <span className={'username'}>
             Приветствую, {user?.first_name}
             </span>
